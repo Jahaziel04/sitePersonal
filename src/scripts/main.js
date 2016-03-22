11905d3f65659df51865f534af6485b09b5984ad
jahaziel <jahaziel0410@hotmail.com>
2016-03-21 22:50:45 -0600

    Primer commit

---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+var $ = require('jquery');
+
+
+//Al precionar el toggle le agrega a su padre la clase active
+$(".toggle").on("click", function() {
+  $(".toggle").parent().toggleClass('active');
+});
+
