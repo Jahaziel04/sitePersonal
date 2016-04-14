9992d66160a4dbdfbebace62a24e0f76f0cc144d
jahaziel <jahaziel0410@hotmail.com>
2016-04-13 22:58:45 -0500

    Razones done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,25 @@ $(".toggle").on("click", function() {
   $(".toggle").parent().toggleClass('active');
 });
 
+///Codigo de la caja de razones a la pregunta
+///¿Por que tener un sitio web?
+$('#check1').on("click", function() {
+	$("#check1").parent().toggleClass('shadow');
+});
+
+$('#check2').on("click", function() {
+	$("#check2").parent().toggleClass('shadow');
+});
+
+$('#check3').on("click", function() {
+	$("#check3").parent().toggleClass('shadow');
+});
+
+$('#check4').on("click", function() {
+	$("#check4").parent().toggleClass('shadow');
+});
+
+$('#check5').on("click", function() {
+	$("#check5").parent().toggleClass('shadow');
+});
+/////Fin del bloque
